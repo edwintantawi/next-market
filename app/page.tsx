3391b4d6da8b7ffae84c9709d53f3fa59a0449f9
edwintantawi <edwintantawi.workspace@gmail.com>
2023-05-07 00:38:12 +0700

    feat: fetch products data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <main></main>;
+import { ProductCard } from '~/components/product-card';
+import { Product } from '~/lib/types';
+
+export default async function Home() {
+  const response = await fetch(`${process.env['BASE_API_URL']}/products`);
+  const products = (await response.json()) as Product[];
+
+  return (
+    <main className="grid grid-cols-2 gap-4 py-8 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </main>
+  );
 }
