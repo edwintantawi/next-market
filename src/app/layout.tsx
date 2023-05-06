eb27d303a7885fa0eb4278e9751cf3e2bb012538
edwintantawi <edwintantawi.workspace@gmail.com>
2023-05-06 23:07:14 +0700

    feat: use navbar component for root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import './globals.css';
+import { Navbar } from '~/components/navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <div className="container">{children}</div>
+      </body>
     </html>
   );
 }
