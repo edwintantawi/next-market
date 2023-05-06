5efd0c46a78b2b664da4d0c7a1a1cd23d3fdae9a
edwintantawi <edwintantawi.workspace@gmail.com>
2023-05-06 23:07:46 +0700

    feat: cleanup page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from '~/components/ui';
-
 export default function Home() {
-  return (
-    <main className="grid h-screen place-content-center gap-4">
-      <Button>Next Market</Button>
-    </main>
-  );
+  return <main></main>;
 }
